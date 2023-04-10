88c6c77a557c775444ea3b090a8aed40a73f697d
Sene0135 <87446887+Sene0135@users.noreply.github.com>
2023-04-10 13:23:46 -0700

    Update auton.cpp

--- src/auton.cpp
+++ src/auton.cpp
@@ -1,9 +1,8 @@
-//#ifndef AUTON_H
-
 #include "auton.h"
 using namespace pros;
 Auton::Auton()
 {
+
   // define motors using their ports
   //1 reverses motor, 0 is normal
   arm_left = new pros::Motor(LEFT_PORT, GEARSET, 1, MTR_UNITS);
@@ -31,149 +30,46 @@ arm_left->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 //  belt->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 }
 
-//REAL: Distance sensor auton
-void Auton::distance() {
-
- pros::ADIUltrasonic distance_sensor (ULTRA_PING, ULTRA_ECHO); //Initialize dist sensor
-
-  t_left->move_velocity(-30);
-  t_right->move_velocity(-30);
-  b_left->move_velocity(-30);
-  b_right->move_velocity(-30);
-  	pros::lcd::set_text(3, std::to_string(distance_sensor.get_value()));
-  pros::delay(1500); //move for a little bit to get enough space to rotates
-
-bool mogodetected = false;
-
-while (mogodetected == false ) { //rotate until goal is detected within range
-if (!(distance_sensor.get_value() < 250 || distance_sensor.get_value() > 500)) {
-  t_left->move_velocity(0);
-  t_right->move_velocity(0);
-  b_left->move_velocity(0);
-  b_right->move_velocity(0);
-  pros::lcd::set_text(3, std::to_string(distance_sensor.get_value()));
-  /*
-  if ((distance_sensor.get_value() < 500 )) {
-    t_left->move_velocity(5);
-    t_right->move_velocity(-5);
-    b_left->move_velocity(5);
-    b_right->move_velocity(-5);
-  pros::delay(2000);
-  mogodetected = true;
-  break;
-}
-*/
-}
-else {
-  	pros::lcd::set_text(3, std::to_string(distance_sensor.get_value()));
-  t_left->move_velocity(10);
-  t_right->move_velocity(-10);
-  b_left->move_velocity(10);
-  b_right->move_velocity(-10);
-  delay(1);
-}
-}
-t_left->move_velocity(0);
-t_right->move_velocity(0);
-b_left->move_velocity(0);
-b_right->move_velocity(0);
-delay(50);
-  arm_left->move_absolute(105, 20); //Drop arms
-  arm_right->move_absolute(105, 20);
-  pros::delay(2000);
-
-  while(distance_sensor.get_value() > 50 ) { //Move back until goal is within range
-  t_left->move_velocity(-15);
-  t_right->move_velocity(-15);
-  b_left->move_velocity(-15);
-  b_right->move_velocity(-15);
-  delay(1);
-}
-
-  delay(1500);
-/*
-  if (!(distance_sensor.get_value() < 200 || distance_sensor.get_value() > 300)) { //if goal is in arms
-    arm_left->move_velocity(-30); //Lift arms(with mogo in hopefully)
-    arm_right->move_velocity(-30);
-    delay(500);
-    if(arm_right->get_efficiency() <= 60) {
-
-      arm_left->move_absolute(30, 1000); //Lift arms(with mogo in hopefully)
-      arm_right->move_absolute(30, 1000);
-      delay(500);
-      t_left->move_velocity(15);
-      t_right->move_velocity(15);
-      b_left->move_velocity(15);
-      b_right->move_velocity(15);
-    }
-
-   else{
-     arm_right->move_velocity(0);
-     arm_left->move_velocity(0);
-   pros::lcd::initialize();
-   pros::lcd::set_text(2, "GOAL NOT DETECTED" );
-  //add countercheck
-    }
-
-  }
-
-  else {
-
-    while(distance_sensor.get_value() > 260 ) { //Move back until goal is within range
-    t_left->move_velocity(-15);
-    t_right->move_velocity(-15);
-    b_left->move_velocity(-15);
-    b_right->move_velocity(-15);
-    delay(100);
-    }
-
-    arm_left->move_absolute(30, 100000); //Lift arms(with mogo in hopefully)
-    arm_right->move_absolute(30, 100000);
-
-  }
-
-*/
-
-}
 
 void Auton::ultrasonic() {
 
 
    pros::ADIUltrasonic distance_sensor (ULTRA_PING, ULTRA_ECHO); //Initialize dist sensor
 
-    t_left->move_velocity(-30);
-    t_right->move_velocity(-30);
-    b_left->move_velocity(-30);
-    b_right->move_velocity(-30);
+    t_left->move_velocity(-40);
+    t_right->move_velocity(-40);
+    b_left->move_velocity(-40);
+    b_right->move_velocity(-40);
     	pros::lcd::set_text(3, std::to_string(distance_sensor.get_value()));
-    pros::delay(1500); //move for a little bit to get enough space to rotates
-    t_left->move_velocity(10);
-    t_right->move_velocity(-10);
-    b_left->move_velocity(10);
-    b_right->move_velocity(-10);
-    delay(500);
+    pros::delay(1000); //move for a little bit to get enough space to rotates
+    t_left->move_velocity(20);
+    t_right->move_velocity(-20);
+    b_left->move_velocity(20);
+    b_right->move_velocity(-20);
+    delay(250);
   bool mogodetected = false;
 
   while (mogodetected == false ) { //rotate until goal is detected within range
-  if (!(distance_sensor.get_value() < 60 || distance_sensor.get_value() > 500)) {
+  if ((distance_sensor.get_value() > 65 && distance_sensor.get_value() < 500)) {
     //int time;
   //  int timer(time);
 
 
-    t_left->move_velocity(10);
-    t_right->move_velocity(-10);
-    b_left->move_velocity(10);
-    b_right->move_velocity(-10);
+    t_left->move_velocity(20);
+    t_right->move_velocity(-20);
+    b_left->move_velocity(20);
+    b_right->move_velocity(-20);
     pros::lcd::set_text(3, std::to_string(distance_sensor.get_value()));
 
 //we
-if (distance_sensor.get_value() > 600) //finds end point of mogo
+if (distance_sensor.get_value() > 550) //finds end point of mogo
 {
-  t_left->move_velocity(-10);
-  t_right->move_velocity(10);
-  b_left->move_velocity(-10);
-  b_right->move_velocity(10);
-  delay(1100); //1200 if perfectly at 0 deg
+
+  t_left->move_velocity(-20);
+  t_right->move_velocity(20);
+  b_left->move_velocity(-20);
+  b_right->move_velocity(20);
+  delay(500); //1200 if perfectly at 0 deg
   break;
   mogodetected = true;
 
@@ -197,227 +93,108 @@ b_right->move_absolute(b_right->get_position()/2, 10);
 
 }
 //move back to goal
-t_left->move_velocity(0);
-t_right->move_velocity(0);
-b_left->move_velocity(0);
-b_right->move_velocity(0);
-arm_left->move_absolute(110, 20); //CHECK efficiency
-arm_right->move_absolute(110, 20);
-//while (distance_sensor.get_value() > 50) {
-  //arm_left->move_velocity(20); //CHECK efficiency
-  //arm_right->move_velocity(20);
-//  delay(1);
-//}
-delay(2000);
-t_left->move_velocity(-15);
-t_right->move_velocity(-15);
-b_left->move_velocity(-15);
-b_right->move_velocity(-15);
-delay(2000);
-
-arm_left->move_velocity(-30); //CHECK efficiency
-arm_right->move_velocity(-30);
-delay(500);
-
-if(arm_right->get_efficiency() < 60) {
-  arm_left->move_absolute(30, 10000000); //Lift arms(with mogo in hopefully)
-  arm_right->move_absolute(30, 100000000);
-}
-
-else if(arm_right->get_efficiency() >= 60){
- arm_right->move_velocity(0);
- arm_left->move_velocity(0);
-pros::lcd::initialize();
-pros::lcd::set_text(2, "GOAL NOT DETECTED" );
-
-}
-
-t_left->move_velocity(0);
-t_right->move_velocity(0);
-b_left->move_velocity(0);
-b_right->move_velocity(0);
-delay(100);
-
-t_left->move_velocity(15);
-t_right->move_velocity(15);
-b_left->move_velocity(15);
-b_right->move_velocity(15);
-delay(700);
-t_left->move_velocity(15);
-t_right->move_velocity(-15);
-b_left->move_velocity(15);
-b_right->move_velocity(-15);
-delay(1600);
-t_left->move_velocity(15);
-t_right->move_velocity(15);
-b_left->move_velocity(15);
-b_right->move_velocity(15);
-belt->move_velocity(600);
-delay(3000);
-exit(EXIT_SUCCESS);
-}
-
-
-
-
-//testing optical SENSOR_PORT
-void Auton::visiontest()
-{
- pros::Vision vision_sensor (VISION_SENSOR, pros::E_VISION_ZERO_CENTER);
-  pros::vision_signature_s_t RED_SIG =
-    pros::Vision::signature_from_utility(EXAMPLE_SIG, 8973, 11143, 10058, -2119, -1053, -1586, 5.4, 0);
-  vision_sensor.set_signature(EXAMPLE_SIG, &RED_SIG);
-vision_object_s_t object_arr[NUM_VISION_OBJECTS];
-
-  //vision_object_s_t rtn = vision_sensor.get_by_size(0);
-      // Gets the largest object
-  //    pros::lcd::set_text(3, std::to_string(rtn.signature));
-    //  std::cout << "sig: " << rtn.signature;
-      pros::delay(2);
-bool foundmogo = false;
-pros::lcd::initialize();
-  pros::c::vision_read_by_sig(VISION_SENSOR, 0, EXAMPLE_SIG, NUM_VISION_OBJECTS, object_arr);
-   printf("sig: %d", object_arr[0].signature);
- if(object_arr[0].signature) {
-    pros::lcd::set_text(4, "DETECTED MOGO!");
-    vision_sensor.get_object_count();
- }
- else {
-    pros::lcd::set_text(4, "NULL");
-  }
-
-
-
-
-}
-
-
-//testing auton motion
-void Auton::test() {
-    //move arm down slowly
-    arm_left->move_absolute(90, 10);
-    arm_right->move_absolute(90, 10);
-    pros::delay(2000);
-
-    //slowly back up into goal
-    t_left->move_velocity(-15);
-    t_right->move_velocity(-15);
-    b_left->move_velocity(-15);
-    b_right->move_velocity(-15);
-    pros::delay(1500); //delay function determines how long the robot moves in the given state
-
-
-    //Move arm back up(hopefully with goal in hand)
-    arm_left->move_absolute(30, 100000);
-    arm_right->move_absolute(30, 100000);
-
-    pros::delay(1000);
-
-    belt->move_velocity(40); // suck up rings in the path
-    t_left->move_velocity(40);
-    t_right->move_velocity(40);
-    b_left->move_velocity(40);
-    b_right->move_velocity(40);
-
-    pros::delay(3000);
-
-    belt->move_velocity(400); // suck up rings in the path
-    t_left->move_velocity(0);
-    t_right->move_velocity(0);
-    b_left->move_velocity(0);
-    b_right->move_velocity(0);
-
-}
-
-
-//testing auton motion 2 (real scenario)
-void Auton::rotate() {
-
-   t_left->move_velocity(-10);
-   t_right->move_velocity(-10);
-   b_left->move_velocity(-10);
-   b_right->move_velocity(-10);
-   pros::delay(2700); //move for a little bit to get enough space to rotates
-
-   //rotate until goal is detected within range
-   t_left->move_velocity(10);
-   t_right->move_velocity(-10);
-   b_left->move_velocity(10);
-   b_right->move_velocity(-10);
-   delay(100);
-
-   arm_left->move_absolute(105, 20); //Drop arms
-   arm_right->move_absolute(105, 20);
-   pros::delay(2000);
-
-//Move back until goal is within range
-   t_left->move_velocity(-15);
-   t_right->move_velocity(-15);
-   b_left->move_velocity(-15);
-   b_right->move_velocity(-15);
-   delay(3000);
-
-   arm_left->move_velocity(-30); //CHECK efficiency
-   arm_right->move_velocity(-30);
-   delay(500);
-
-   if(arm_right->get_efficiency() < 60) {
-     arm_left->move_absolute(30, 10000000); //Lift arms(with mogo in hopefully)
-     arm_right->move_absolute(30, 100000000);
-   }
-
-  else if(arm_right->get_efficiency() >= 60){
-    arm_right->move_velocity(0);
-    arm_left->move_velocity(0);
-  pros::lcd::initialize();
-  pros::lcd::set_text(2, "GOAL NOT DETECTED" );
-
-   }
-
-   t_left->move_velocity(0);
-   t_right->move_velocity(0);
-   b_left->move_velocity(0);
-   b_right->move_velocity(0);
-
-   pros::delay(500);
-
-   t_left->move_velocity(15);
-   t_right->move_velocity(15);
-   b_left->move_velocity(15);
-   b_right->move_velocity(15);
-  pros::delay(1000);
-
+  t_left->move_velocity(0);
+  t_right->move_velocity(0);
+  b_left->move_velocity(0);
+  b_right->move_velocity(0);
+  arm_left->move_absolute(-295, 40); //CHECK efficiency
+  arm_right->move_absolute(-295, 40);
+  //while (distance_sensor.get_value() > 50) {
+    //arm_left->move_velocity(20); //CHECK efficiency
+    //arm_right->move_velocity(20);
+  //  delay(1);
+  //}
+  delay(2300);
   t_left->move_velocity(15);
   t_right->move_velocity(15);
   b_left->move_velocity(15);
   b_right->move_velocity(15);
- pros::delay(1000);
+  delay(300);
+  t_left->move_velocity(-25);
+  t_right->move_velocity(-25);
+  b_left->move_velocity(-25);
+  b_right->move_velocity(-25);
+  delay(1500);
+  
+  arm_left->move_absolute(-90, 80);
+  arm_right->move_absolute(-90, 80);
+  delay(600);
 
-   }
 
 
- //gauges whether mogo is in arms
-void Auton::motortest() {
-
-std::cout << arm_right->get_efficiency();
-     arm_left->move_absolute(90, 10); //Lift arms(with mogo in hopefully)
-     arm_right->move_absolute(90, 10);
-     delay(5000);
-     arm_left->move_velocity(-30); //Lift arms(with mogo in hopefully)
-     arm_right->move_velocity(-30);
-     delay(500);
-std::cout << arm_right->get_efficiency();
-   if(arm_right->get_efficiency() < 60) {
-     arm_left->move_absolute(30, 1000); //Lift arms(with mogo in hopefully)
-     arm_right->move_absolute(30, 1000);
-   }
-
-  else if(arm_right->get_efficiency() >= 60){
-    arm_right->move_velocity(0);
-    arm_left->move_velocity(0);
-  pros::lcd::initialize();
-  pros::lcd::set_text(2, "GOAL NOT DETECTED" );
-
-   }
-
-   }
+  t_left->move_velocity(0);
+  t_right->move_velocity(0);
+  b_left->move_velocity(0);
+  b_right->move_velocity(0);
+  delay(20);
+  t_left->move_velocity(-11); //rotates to rings
+  t_right->move_velocity(11);
+  b_left->move_velocity(-11);
+  b_right->move_velocity(11);
+  delay(1000);
+  t_left->move_velocity(20); // moves forward a bit after picking up goal
+  t_right->move_velocity(20);
+  b_left->move_velocity(20);
+  b_right->move_velocity(20);
+  delay(1600);
+  t_left->move_velocity(15); //rotates to rings
+  t_right->move_velocity(-15);
+  b_left->move_velocity(15);
+  b_right->move_velocity(-15);
+  delay(2700);
+  t_left->move_velocity(15);
+  t_right->move_velocity(15);
+  b_left->move_velocity(15);
+  b_right->move_velocity(15);
+  belt->move_velocity(69000);
+  delay(6600);
+  t_left->move_velocity(-20);
+  t_right->move_velocity(-20);
+  b_left->move_velocity(-20);
+  b_right->move_velocity(-20);
+  delay(5500);
+  t_left->move_velocity(0);
+  t_right->move_velocity(0);
+  b_left->move_velocity(0);
+  b_right->move_velocity(0);
+  belt->move_velocity(0);
+  arm_left->move_absolute(-295, 40);
+  arm_right->move_absolute(-295, 40);
+  delay(2000);
+  t_left->move_velocity(25);
+  t_right->move_velocity(25);
+  b_left->move_velocity(25);
+  b_right->move_velocity(25);
+  belt->move_velocity(250000);
+  delay(2800);
+  t_left->move_velocity(0);
+  t_right->move_velocity(0);
+  b_left->move_velocity(0);
+  b_right->move_velocity(0);
+  belt->move_velocity(0);
+  arm_left->move_absolute(-90, 80);
+  arm_right->move_absolute(-90, 80);
+  delay(300);
+/*
+t_left->move_velocity(20);
+t_right->move_velocity(-20);
+b_left->move_velocity(20);
+b_right->move_velocity(-20);
+delay(1400);
+t_left->move_velocity(-50);
+t_right->move_velocity(-50);
+b_left->move_velocity(-50);
+b_right->move_velocity(-50);
+delay(2500);
+t_left->move_velocity(-45);
+t_right->move_velocity(45);
+b_left->move_velocity(-45);
+b_right->move_velocity(45);
+delay(700);
+t_left->move_velocity(0);
+t_right->move_velocity(0);
+b_left->move_velocity(0);
+b_right->move_velocity(0);
+delay(200000);
+*/
+}
